73f78f493c61a0e5663c2f4cdc1257a753327f74
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-24 12:59:05 -0300

    feat: create global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import { AppProps } from 'next/app'
+import '../styles/global.scss';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
