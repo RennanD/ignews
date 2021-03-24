a50e076aa41c3c7cf7a3432b848c2ca0dab7d822
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-24 13:21:46 -0300

    feat: create header application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/global.scss';
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
