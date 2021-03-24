7c3e9ddf0b91bf5573af346f23187eebde552048
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-24 12:43:55 -0300

    feat: create custom document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <>
+    <Head>
+      <title>Início | Ig.News</title>
+    </Head>
+    <h1>
+      Hello world
+    </h1>
+    </>
   )
 }
