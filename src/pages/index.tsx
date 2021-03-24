b38463ca5fe9bc6173e95fa29d66ddeb95879a19
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-24 14:02:05 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,21 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Início | Ig.News</title>
-    </Head>
-    <h1>
-      Hello world
-    </h1>
+      <Head>
+        <title>Home | Ig.News</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome 👏</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to publications <br />
+            <span>For $9.90</span>
+          </p>
+
+        </section>
+        <img src="/images/avatar.svg" alt="Girl code"/>
+      </main>
     </>
   )
 }
