d46e6f66bea19aac7da2f71c6f4af9c8d951b4db
rennand <rplaykl@gmail.com>
2021-03-24 21:29:29 -0300

    refactor: change home to statc site - next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import  { GetServerSideProps } from 'next'
+import  { GetStaticProps } from 'next'
 
 import styles from '../styles/home.module.scss';
 import Head from 'next/head'
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1IYg7MAxvcIhZogcb5AWzLWB');
 
@@ -47,6 +47,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 };
