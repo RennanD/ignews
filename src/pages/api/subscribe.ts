9ba734a674f5241d71bde3366333d3ec3a7bf8e3
rennand <rplaykl@gmail.com>
2021-03-29 13:02:03 -0300

    feat: create only user on fauna and stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,7 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 
+import { query } from 'faunadb'
+
+import { fauna } from "../../services/fauna";
+
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  },
+  data: {
+    stripe_customer_id: string;
+  }
+}
 
 export default async ( request: NextApiRequest, response: NextApiResponse) => {
   if (request.method === 'POST') {
@@ -9,11 +22,37 @@ export default async ( request: NextApiRequest, response: NextApiResponse) => {
     try {
       const session = await getSession({
         req: request
-      })    
+      });
   
-      const stripeCustomer = await stripe.customers.create({
-        email: session.user.email
-      })
+      const user = await fauna.query<User>(
+        query.Get(
+          query.Match(
+            query.Index('user_by_email'),
+            query.Casefold(session.user.email)
+          )
+        )
+      );
+
+      let customerId = user.data.stripe_customer_id;
+
+      if(!customerId) {
+        const stripeCustomer = await stripe.customers.create({
+          email: session.user.email
+        });
+  
+        await fauna.query(
+          query.Update(
+            query.Ref( query.Collection('users'), user.ref.id), 
+            {
+              data: {
+                stripe_customer_id: stripeCustomer.id
+              }
+            }
+          )
+        );
+
+        customerId = stripeCustomer.id;
+      }
   
       const stripeCheckoutSession = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
@@ -28,7 +67,7 @@ export default async ( request: NextApiRequest, response: NextApiResponse) => {
         allow_promotion_codes: true,
         success_url: process.env.STRIPE_SUCCESS_URL,
         cancel_url: process.env.STRIPE_CANCEL_URL,
-        customer: stripeCustomer.id
+        customer: customerId
       })
   
       return response.status(200).json({
