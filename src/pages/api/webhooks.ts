8428c903ef286c98c02009ed81cdf3e53d14449e
rennand <rplaykl@gmail.com>
2021-03-29 13:51:13 -0300

    feat: listen webhook events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,8 +1,57 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+import { Readable } from 'stream';
+
+import { stripe } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+  
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
 
 export default async ( request: NextApiRequest, response: NextApiResponse) => {
+  if(request.method === 'POST') {
+    const buf = await buffer(request);
+    const secret = request.headers['stripe-signature'];
 
-  console.log('chegou o evento');
+    let event: Stripe.Event;
 
-  return response.status(200).json({ ok: true });
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf, 
+        secret, 
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      return response.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    const { type } = event;
+
+    if(relevantEvents.has(type)) {
+      console.log('Evento recebido', event);
+    }
+
+    return response.status(200).json({ ok: true });
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed');
+  }
 }
